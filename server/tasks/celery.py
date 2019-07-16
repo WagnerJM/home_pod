57b27f81e2df7750816c77a02731bccf72a21daf
Jean-Marc Wagner <jeanmarc.wagner@gmx.de>
2019-07-16 20:32:03 +0200

    reset des commits

--- server/tasks/celery.py
+++ server/tasks/celery.py
@@ -7,8 +7,6 @@ from email.message import EmailMessage
 REDIS_URL = 'redis://:{pw}@redis:6379/0'.format(pw=os.getenv('REDIS_PW'))
 BROKER_URL = 'redis://:{pw}@redis:6379/1'.format(pw=os.getenv('REDIS_PW'))
 
-
-
 celery = Celery('tasks', backend=REDIS_URL, broker=BROKER_URL)
 
 
